8e76b443d6d410cb99446fa5331f9beb3def9ed8
Takuma-Shousuke <shousukesketty@gmail.com>
2020-10-09 14:15:35 +0900

    Hariのスクリプトを追加

---
+++ Hari.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RunGame.Stage
+{
+    public class Hari : MonoBehaviour
+    {
+        public float deleteTime = 5.0f;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Destroy(gameObject, deleteTime);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // float speed = 0.05f;　このスピードが元
+            float speed = 0.05f;
+            transform.Translate(-speed, -speed, 0.0f);
+            Debug.Log("speed = " + speed);
+        }
+        void FixedUpdate()
+        {
+
+        }
+        void OnTiriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag == "Ground")
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
